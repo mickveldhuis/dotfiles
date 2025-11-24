bd201e34483e648344ecda156dd86ee164f3b54a
Mick Veldhuis <mickveldhuis@hotmail.nl>
2025-11-24 11:28:51 +0100

    Add zoxide to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,18 @@
-# Commands to run in interactive sessions
+###########################################
+# Commands to run in interactive sessions #
+###########################################
 if status is-interactive
     # ???
 end
 
-# Additional exports
-# Example: set -x PATH /home/mick/bin $PATH
+### Additional exports ######################
+# Example: set -x PATH /home/mick/bin $PATH #
+#############################################
 set -g fish_greeting
+
+### Initialise zoxide ##############################
+# Initialise zoxide if available, ignore otherwise #
+####################################################
+if type -q zoxide
+    zoxide init fish | source
+end
